de1e52c0588eee0ce8be7eb9c67e006165b28839
Roman <romankashta4ik@gmail.com>
2026-05-24 09:08:45 +0300

    Добавлены DTO для авторизации

---
+++ src/main/kotlin/presentation/dto/AuthDtos.kt
@@ -0,0 +1,23 @@
+package com.example.presentation.dto
+
+import com.example.domain.model.User
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class RegisterRequest(
+    val email: String,
+    val password: String,
+    val name: String
+)
+
+@Serializable
+data class LoginRequest(
+    val email: String,
+    val password: String
+)
+
+@Serializable
+data class AuthResponse(
+    val token: String,
+    val user: User
+)
